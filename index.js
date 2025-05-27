a954f852726d60b7e3052b0eb98767435add4421
geomer simanca <simancanavarrogeomerandres@gmail.com>
2025-05-27 12:09:43 -0500

    modificacion de puertos para render

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 4000 ;
 
 // middleware
 app.use(express.static('public'))
